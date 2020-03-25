8a4d3896da1bae07da176a41f13578c3320d95f2
Chris <chris@ladybug.tools>
2020-03-25 13:05:41 -0400

    fix(transform): Fix component outputs to have correct name
    None of these components were outputting the transformed zones. This fixes the issue.

--- honeybee_grasshopper_core/src/HB Mirror.py
+++ honeybee_grasshopper_core/src/HB Mirror.py
@@ -33,7 +33,7 @@ Mirror any Honeybee geometry object or a Model across a plane.
 
 ghenv.Component.Name = "HB Mirror"
 ghenv.Component.NickName = 'Mirror'
-ghenv.Component.Message = '0.1.1'
+ghenv.Component.Message = '0.1.2'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '0 :: Create'
 ghenv.Component.AdditionalHelpFromDocStrings = "6"
