e6d786a9e17fa150f9920d82a29407d81b73afce
Chris <chris@ladybug.tools>
2020-07-18 22:01:02 -0400

    fix(create): Fix Point3d type hint on honeybee scale component

--- honeybee_grasshopper_core/src/HB Scale.py
+++ honeybee_grasshopper_core/src/HB Scale.py
@@ -38,7 +38,7 @@ Scale any Honeybee geometry object or a Model by a factor.
 
 ghenv.Component.Name = "HB Scale"
 ghenv.Component.NickName = 'Scale'
-ghenv.Component.Message = '0.1.3'
+ghenv.Component.Message = '0.1.4'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '0 :: Create'
 ghenv.Component.AdditionalHelpFromDocStrings = "6"
