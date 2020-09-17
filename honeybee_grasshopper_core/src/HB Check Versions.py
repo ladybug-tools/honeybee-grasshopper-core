8f487bca686b2dddd89692ccc29df22fd30940cd
Chris <chris@ladybug.tools>
2020-09-16 21:26:11 -0400

    feat(visualize): Add component to check versions of engines
    Will be very helpful for debugging.

---
+++ honeybee_grasshopper_core/src/HB Check Versions.py
@@ -0,0 +1,54 @@
+# Honeybee: A Plugin for Environmental Analysis (GPL)
+# This file is part of Honeybee.
+#
+# Copyright (c) 2019, Ladybug Tools.
+# You should have received a copy of the GNU General Public License
+# along with Honeybee; If not, see <http://www.gnu.org/licenses/>.
+# 
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""
+Check the versions of the engines that are being used by the honeybee plugin.
+This is useful for verifying that everything has been installed correctly
+and that the versions of the engines are as expected.
+-
+
+    Returns:
+        python: The version of Python used for Ladybug Tools CLI calls.
+        radiance: The version of Radiance if it is installed.
+        openstudio: The version of OpenStudio if it is installed.
+        energyplus: The version of EnergyPlus if it is installed.
+"""
+
+ghenv.Component.Name = 'HB Check Versions'
+ghenv.Component.NickName = 'Versions'
+ghenv.Component.Message = '0.1.0'
+ghenv.Component.Category = 'Honeybee'
+ghenv.Component.SubCategory = '1 :: Visualize'
+ghenv.Component.AdditionalHelpFromDocStrings = '0'
+
+try:  # import the core honeybee dependencies
+    from honeybee.config import folders as hb_folders
+except ImportError as e:
+    raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
+
+try:
+    from honeybee_radiance.config import folders as radiance_folders
+    radiance_loaded = True
+except ImportError as e:
+    radiance_loaded = False
+
+try:
+    from honeybee_energy.config import folders as energy_folders
+    energy_loaded = True
+except ImportError:
+    energy_loaded = False
+
+
+# output the versions
+python = hb_folders.python_version_str
+if radiance_loaded:
+    radiance = radiance_folders.radiance_version_str
+if energy_loaded:
+    openstudio = energy_folders.openstudio_version_str
+    energyplus = energy_folders.energyplus_version_str
