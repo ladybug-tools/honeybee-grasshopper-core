a0c8cae7a0042ac23e25df99e6426107cd44c65e
Chris Mackey <chris@ladybug.tools>
2021-02-12 14:32:56 -0500

    feat(recipe): Refactor "Run Recipe" component to use queenbee local
    The update should mean that things are much more reusable and it will be a lot faster to implement new recipes going forward.

--- honeybee_grasshopper_core/src/HB Run Recipe.py
+++ honeybee_grasshopper_core/src/HB Run Recipe.py
@@ -8,18 +8,16 @@
 # @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 
 """
-Execute any Queenbee Recipe on this machine using queenbee-luigi.
+Execute any Recipe on this machine using queenbee-luigi.
 
 -
     Args:
-        _recipe: A Queenbee recipe object generated from any Queenbee recipe component.
+        _recipe: A recipe object generated from any recipe component.
         _folder_: An optional folder out of which the recipe will be executed.
-            NOTE THAT DIRECTORIES INPUT HERE SHOULD NOT HAVE ANY SPACES OR
-            UNDERSCORES IN THE FILE PATH.
         _cpu_count_: An integer to set the number of CPUs used in the execution
             of the recipe. This number should not exceed the number of CPUs on
             the machine running the simulation and should be lower if other tasks
-            are running while the simulation is running.(Default: 2).
+            are running while the simulation is running. (Default: 2).
         reload_old_: A boolean to indicate whether existing results for a given
             model and recipe should be reloaded if they are found instead of
             re-running the entire recipe from the beginning. If False or
@@ -31,15 +29,15 @@ Execute any Queenbee Recipe on this machine using queenbee-luigi.
             recipe reports can often be very long and so it can slow
             Grasshopper slightly. (Default: False).
         _run: Set to "True" to run the recipe.
-    
+
     Returns:
         report: Reports, errors, warnings, etc.
-        results: A list of results output from the recipe.
+        results: The results output from the recipe.
 """
 
 ghenv.Component.Name = 'HB Run Recipe'
 ghenv.Component.NickName = 'RunRecipe'
-ghenv.Component.Message = '1.1.1'
+ghenv.Component.Message = '1.1.2'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '4 :: Simulate'
 ghenv.Component.AdditionalHelpFromDocStrings = '1'
@@ -60,28 +58,24 @@ except ImportError as e:
 try:
     from honeybee_radiance.config import folders as rad_folders
 except ImportError as e:
-    raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
+    raise ImportError('\nFailed to import honeybee_radiance:\n\t{}'.format(e))
 
 try:
-    from ladybug_rhino.grasshopper import all_required_inputs
+    from ladybug_rhino.grasshopper import all_required_inputs, give_warning, \
+        list_to_data_tree
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
 
-
-def sort_results_by_grid(results, grid_names):
-    """Sort result files according to the order that they appear in a list of grids."""
-    file_dict = {}  # dictionary relating grid name to file name
-    for res_file in results:
-        grid_name = '.'.join(os.path.basename(res_file).split('.')[:-1])
-        file_dict[grid_name] = res_file
-    grid_files = []
-    for g_name in grid_names:
-        try:
-            grid_files.append(file_dict[g_name])
-        except KeyError:  # grid that was not simulated
-            pass
-    other_files = [val for key, val in file_dict.items() if key not in grid_names]
-    return other_files + grid_files
+# check the installed Radiance and ensure it's from the right date
+compatible_rad_date = (2020, 9, 3)
+hb_url = 'https://github.com/ladybug-tools/lbt-grasshopper/wiki/1.4-Compatibility-Matrix'
+rad_msg = 'Download and install the version of Radiance listed in the Ladybug ' \
+    'Tools compatibility matrix\n{}'.format(hb_url)
+assert rad_folders.radiance_path is not None, \
+    'No Radiance installation was found on this machine.\n{}'.format(rad_msg)
+assert rad_folders.radiance_version_date >= compatible_rad_date, \
+    'The installed Radiance is not from {} or later.' \
+    '\n{}'.format('/'.join(str(v) for v in compatible_rad_date), rad_msg)
 
 
 if all_required_inputs(ghenv.Component) and _run:
@@ -90,11 +84,7 @@ if all_required_inputs(ghenv.Component) and _run:
 
     # get the folder out of which the recipe will be executed
     if _folder_ is None:
-        if _recipe.default_simulation_path is not None:
-            _folder_ = _recipe.default_simulation_path
-        else:  # no default simulation path
-            _folder_ = os.path.join(
-                hb_folders.default_simulation_folder, 'unnamed_recipe')
+        _folder_ = _recipe.default_project_folder
     if not os.path.isdir(_folder_):
         preparedir(_folder_)  # create the directory if it's not there
 
@@ -104,25 +94,25 @@ if all_required_inputs(ghenv.Component) and _run:
         if os.path.isdir(wf_folder):
             nukedir(wf_folder, rmdir=True)
 
-    # write the inputs JSON for the recipe
+    # write the inputs JSON for the recipe and set up the envrionment variables
     inputs_json = _recipe.write_inputs_json(_folder_)
-
     genv = {}
     genv['PATH'] = rad_folders.radbin_path
     genv['RAYPATH'] = rad_folders.radlib_path
     env_args = ['--env {}="{}"'.format(k, v) for k, v in genv.items()]
 
     # create command
-    command = '"{hb_recipe}" run {recipe_name} ' \
+    command = '"{qb_path}" local run "{recipe_folder}" ' \
         '"{project_folder}" -i "{user_inputs}" --workers {cpu_count} ' \
         '{environment} --name {simulation_name}'.format(
-            hb_recipe=os.path.join(hb_folders.python_scripts_path, 'honeybee-recipe'),
-            recipe_name=_recipe.name, project_folder=_folder_,
+            qb_path=os.path.join(hb_folders.python_scripts_path, 'queenbee'),
+            recipe_folder=_recipe.path, project_folder=_folder_,
             user_inputs=inputs_json, cpu_count=_cpu_count_,
             environment=' '.join(env_args),
             simulation_name=_recipe.simulation_id
         )
 
+    # execute command
     if report_out_:
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         result = process.communicate()
@@ -130,13 +120,14 @@ if all_required_inputs(ghenv.Component) and _run:
         print result[1]
     else:
         process = subprocess.Popen(command)
-        result = process.communicate()
+        result = process.communicate()  # freeze the canvas while running
 
     # try to parse the results
-    if _recipe.simulation_id:
-        res_folder = os.path.join(_folder_, _recipe.simulation_id, 'results')
-        if os.path.isdir(res_folder):
-            results = [os.path.join(res_folder, fn) for fn in os.listdir(res_folder)]
-            grid_studies = ('annual-daylight', 'daylight-factor')
-            if 'sensor-grids' in _recipe.inputs_dict and _recipe.name in grid_studies:
-                results = sort_results_by_grid(results, _recipe.inputs_dict['sensor-grids'])
+    try:
+        results = _recipe.output_value_by_name('results', _folder_)
+        if isinstance(results, (list, tuple)):
+            results = list_to_data_tree(results)
+    except (AssertionError, ValueError) as e:
+        msg = 'Simulation did not succeed. Try setting reoprt_out_ to True to ' \
+            'see the full traceback.\n{}'.format(e)
+        give_warning(ghenv.Component, msg)
