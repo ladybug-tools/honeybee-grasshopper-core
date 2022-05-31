fa99eded339b9cd0e3a592ee5b3b48e7a7357da4
Chris Mackey <chris@ladybug.tools>
2022-05-31 09:17:51 -0400

    fix(visualize): Add a means to check the installed LBT-Grasshopper

--- honeybee_grasshopper_core/src/HB Check Versions.py
+++ honeybee_grasshopper_core/src/HB Check Versions.py
@@ -14,15 +14,19 @@ and that the versions of the engines are as expected.
 -
 
     Returns:
+        lbt_gh: The version of Ladybug Tools for Grasshopper that is installed.
         python: The version of Python used for Ladybug Tools CLI calls.
-        radiance: The version of Radiance if it is installed.
-        openstudio: The version of OpenStudio if it is installed.
-        energyplus: The version of EnergyPlus if it is installed.
+        radiance: The version of Radiance used by Ladybug Tools (will be None if
+            it is not installed).
+        openstudio: The version of OpenStudio used by Ladybug Tools (will be None if
+            it is not installed).
+        energyplus: The version of EnergyPlus used by Ladybug Tools (will be None if
+            it is not installed).
 """
 
 ghenv.Component.Name = 'HB Check Versions'
 ghenv.Component.NickName = 'Versions'
-ghenv.Component.Message = '1.4.0'
+ghenv.Component.Message = '1.4.1'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '1 :: Visualize'
 ghenv.Component.AdditionalHelpFromDocStrings = '0'
@@ -31,6 +35,12 @@ try:  # import the core honeybee dependencies
     from honeybee.config import folders as hb_folders
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
+
+try:
+    from ladybug_rhino.config import folders as lbr_folders
+    lbr_loaded = True
+except ImportError as e:
+    lbr_loaded = False
 
 try:
     from honeybee_radiance.config import folders as radiance_folders
@@ -47,6 +57,8 @@ except ImportError:
 
 # output the versions
 python = hb_folders.python_version_str
+if lbr_loaded:
+    lbt_gh = lbr_folders.lbt_grasshopper_version_str
 if radiance_loaded:
     radiance = radiance_folders.radiance_version_str
 if energy_loaded:
