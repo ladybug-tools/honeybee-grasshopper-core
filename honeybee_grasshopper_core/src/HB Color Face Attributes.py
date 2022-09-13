150dfa305960a1a8a53f5a3dffe8789f8e684e26
Chris Mackey <chris@ladybug.tools>
2022-09-13 12:31:55 -0400

    fix(visualize): Enable Face attributes for U/R Factors to be Visualized
    These properties are sensitive to the Rhino model units.

--- honeybee_grasshopper_core/src/HB Color Face Attributes.py
+++ honeybee_grasshopper_core/src/HB Color Face Attributes.py
@@ -26,7 +26,7 @@ different faces.
             the face and sub-face attributes of that extension.
         legend_par_: An optional LegendParameter object to change the display
             of the colored faces and sub-faces (Default: None).
-    
+
     Returns:
         mesh: Meshes of the faces and sub-faces colored according to their attributes.
         legend: Geometry representing the legend for colored meshes.
@@ -40,15 +40,22 @@ different faces.
 
 ghenv.Component.Name = 'HB Color Face Attributes'
 ghenv.Component.NickName = 'ColorFaceAttr'
-ghenv.Component.Message = '1.5.0'
+ghenv.Component.Message = '1.5.1'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '1 :: Visualize'
 ghenv.Component.AdditionalHelpFromDocStrings = '3'
 
+try:
+    from ladybug.graphic import GraphicContainer
+except ImportError as e:
+    raise ImportError('\nFailed to import ladybug:\n\t{}'.format(e))
+
 try:  # import the core honeybee dependencies
     from honeybee.model import Model
     from honeybee.room import Room
+    from honeybee.face import Face
     from honeybee.colorobj import ColorFace
+    from honeybee.search import get_attr_nested
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
 
@@ -57,9 +64,17 @@ try:  # import the ladybug_rhino dependencies
         from_face3d_to_wireframe
     from ladybug_rhino.fromobjects import legend_objects
     from ladybug_rhino.color import color_to_color
+    from ladybug_rhino.config import units_system
     from ladybug_rhino.grasshopper import all_required_inputs
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
+
+# dictionary of unit-sensitive propperties to be handled specially
+UNIT_SENSITIVE = {
+    'properties.energy.r_factor': 'R Factor',
+    'properties.energy.u_factor': 'U Factor',
+    'properties.energy.shgc': 'SHGC'
+}
 
 
 if all_required_inputs(ghenv.Component):
@@ -80,14 +95,35 @@ if all_required_inputs(ghenv.Component):
         else:
             faces.append(hb_obj)
 
-    # create the ColorFace visualization object and output geometry
+    # create the ColorFace visualization object
     color_obj = ColorFace(faces, _attribute, legend_par_)
-    graphic = color_obj.graphic_container
+    # if the U-factor is requested, compute it in a unit-sensitive way
+    if _attribute in UNIT_SENSITIVE:
+        units = units_system()
+        values, flat_geo = [], []
+        for face_obj in color_obj.flat_faces:
+            try:
+                obj_method = get_attr_nested(face_obj, _attribute, cast_to_str=False)
+                values.append(obj_method(units))
+                if isinstance(face_obj, Face):
+                    flat_geo.append(face_obj.punched_geometry)
+                else:
+                    flat_geo.append(face_obj.geometry)
+            except TypeError:
+                pass  # shade geometry
+        l_par = color_obj.legend_parameters.duplicate()
+        l_par.title = UNIT_SENSITIVE[_attribute]
+        graphic = GraphicContainer(values, color_obj.min_point, color_obj.max_point, l_par)
+    else:
+        graphic = color_obj.graphic_container
+        values = color_obj.attributes_original
+        flat_geo = color_obj.flat_geometry
+
+    # output the visualization geometry
     mesh = [from_face3ds_to_colored_mesh([fc], col) for fc, col in
-            zip(color_obj.flat_geometry, graphic.value_colors)]
+            zip(flat_geo, graphic.value_colors)]
     wire_frame = []
     for face in color_obj.flat_faces:
         wire_frame.extend(from_face3d_to_wireframe(face.geometry))
     legend = legend_objects(graphic.legend)
-    values = color_obj.attributes_original
     colors = [color_to_color(col) for col in graphic.value_colors]
