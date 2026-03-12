d87efaaa477417bda2436b9d08020f55c2c27a20
Chris Mackey <chris@ladybug.tools>
2026-03-11 17:25:39 -0700

    fix(config): Update config component to no longer have gem output

--- honeybee_grasshopper_core/src/HB Config.py
+++ honeybee_grasshopper_core/src/HB Config.py
@@ -18,25 +18,22 @@ and that the engines are configured as expected.
             Tools CLI calls.
         py_lib_install: The path to where the Ladybug Tools Python packages
             are installed.
+        default_sim: The path to the default simulation folder (where simulation
+            files are written if not specified).
         rad_install: The path to Radiance installation folder if it exists.
         os_install: The path to OpenStudio installation folder if it exists.
         ep_install: The path to EnergyPlus installation folder if it exists.
-        hb_os_gem: The path to the honeybee_openstudio_gem if it exists. This gem
-            contains libraries and measures for translating between Honeybee
-            JSON schema and OpenStudio Model schema (OSM).
         standards: The path to the library of standards if it exists. This library
             contains the default Modifiers, ModifierSets, Constructions,
             ConstructionSets, Schedules, and ProgramTypes. It can be extended
             by dropping IDF or Honeybee JOSN files into the appropriate sub-folder.
         asset_report: A report of all the assets that have been loaded from the
             standards library.
-        default_sim: The path to the default simulation folder (where simulation
-            files are written if not specified by the user.).
 """
 
 ghenv.Component.Name = 'HB Config'
 ghenv.Component.NickName = 'Config'
-ghenv.Component.Message = '1.10.0'
+ghenv.Component.Message = '1.10.1'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '1 :: Visualize'
 ghenv.Component.AdditionalHelpFromDocStrings = '0'
@@ -95,7 +92,6 @@ if radiance_loaded:  # output all of the paths to radiance_folders
 if energy_loaded:  # output all of the paths to energy_folders
     os_install = energy_folders.openstudio_path
     ep_install = energy_folders.energyplus_path
-    hb_os_gem = energy_folders.honeybee_openstudio_gem_path
     standards = energy_folders.standards_data_folder
 
     # generate a report of all the resources loaded from the library
