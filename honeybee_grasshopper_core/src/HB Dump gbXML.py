2db43d921c088509f3c39ebe1e5291f474d21689
Chris Mackey <chris@ladybug.tools>
2026-08-18 13:45:30 -0700

    feat(serialize): Change gbxml component to use new native exporter
    This also exposes a new IP input on the gbXML component.

--- honeybee_grasshopper_core/src/HB Dump gbXML.py
+++ honeybee_grasshopper_core/src/HB Dump gbXML.py
@@ -24,24 +24,37 @@ model geometry and properties.
             If unspecified, it will be derived from the model identifier.
         _folder_: An optional directory into which the honeybee objects will be
             written.  The default is set to the default simulation folder.
-        int_floors_: A boolean to note whether all interior horizontal faces should
-            be written with the InteriorFloor type instead of the combination
-            of InteriorFloor and Ceiling that happens by default with OpenStudio
-            gbXML serialization. (Default: False).
-        triangulate_: Boolean to note whether sub-faces (including Apertures and Doors)
-            should be triangulated if they have more than 4 sides (True) or
-            whether they should be left as they are (False). This triangulation
-            is necessary when exporting directly to EnergyPlus since it cannot
-            accept sub-faces with more than 4 vertices. However, it is not a
-            general requirement of gbXML or all of the simulation engines that
-            gbXML can import to/from. (Default: False).
-        full_geo_: Boolean to note whether space boundaries and shell geometry should
-            be included in the exported gbXML vs. just the minimal required
-            non-manifold geometry. Setting to True to include the full geometry
-            will increase file size without adding much new infomration that
-            doesn't already exist in the file. However, some gbXML interfaces
-            need this geometry in order to properly represent and display
-            room volumes. (Default: False).
+        ip_units_: A boolean to note whether the geometry, space loads, and
+            construction properties are reported in IP units (True) or SI
+            units (False). (Default: False).
+        full_geo_: Boolean to note whether space boundaries and shell geometry are
+            included in the exported gbXML vs. just the minimal required
+            non-manifold Surface geometry. Setting to True increases file
+            size without adding much new infomration beyond that which is
+            already described under the Surface geometry. However, some gbXML
+            interfaces need these extra "copies" of the geometry in order to
+            properly represent and display room volumes. (Default: False).
+        _rect_format_: Text to note how the rectangular geometry for all Surfaces
+            is written into the gbXML. BoundingRectangle sets the width
+            and height of the rectangular geometry using the bounding
+            rectangle around the geometry, which results in an overestimated
+            area for non-rectangular geo. SimpleArea will set the rectangle width
+            always equal to geometry area and the height always equal to one,
+            ensuring accurate areas and making it easy to check the geometry
+            area in the gbXML. SimpleAreaForNonRectOnly will report the width and
+            height of rectangular Face3D correctly but use simpler areas
+            for non-rectangular geometry. (Default: BoundingRectangle). Choose
+            from the following.
+            * BoundingRectangle
+            * SimpleArea
+            * SimpleAreaForNonRectOnly
+        reset_ids_: Boolean to note whether a cleaned version of geometry display
+            names should be used for the IDs that appear within the gbXML file.
+            Setting to True will generally result in more read-able IDs in the
+            gbXML file but this means that it will not be easy to map results
+            back to the input Model. Cases of duplicate IDs resulting from
+            non-unique names will be resolved by adding integers to the ends
+            of the new IDs that are derived from the name. (Default: False).
         _dump: Set to "True" to save the honeybee model to a gbXML file.
 
     Returns:
@@ -51,7 +64,7 @@ model geometry and properties.
 
 ghenv.Component.Name = 'HB Dump gbXML'
 ghenv.Component.NickName = 'DumpGBXML'
-ghenv.Component.Message = '1.10.0'
+ghenv.Component.Message = '1.10.1'
 ghenv.Component.Category = 'Honeybee'
 ghenv.Component.SubCategory = '3 :: Serialize'
 ghenv.Component.AdditionalHelpFromDocStrings = '4'
@@ -68,14 +81,9 @@ except ImportError as e:
     raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
 
 try:
-    from honeybee_openstudio.writer import model_to_gbxml
-except (ImportError, AssertionError):  # openstudio .NET bindings are not available
-    model_to_gbxml = None
-
-try:
-    from lbt_recipes.version import check_openstudio_version
+    from honeybee_energy.writer import model_to_gbxml
 except ImportError as e:
-    raise ImportError('\nFailed to import lbt_recipes:\n\t{}'.format(e))
+    raise ImportError('\nFailed to import honeybee_energy:\n\t{}'.format(e))
 
 try:  # import the core ladybug_rhino dependencies
     from ladybug_rhino.config import folders as lbr_folders
@@ -85,57 +93,36 @@ except ImportError as e:
 
 
 if all_required_inputs(ghenv.Component) and _dump:
-    # check the presence of openstudio and check that the version is compatible
-    check_openstudio_version()
-
-    # check the input and set the component defaults
+    # check the input model and name zones differently from rooms
     assert isinstance(_model, Model), \
         'Excpected Honeybee Model object. Got {}.'.format(type(_model))
+    model = _model.duplicate()
+    if bool(reset_ids_):
+        model.reset_ids()
+    for room in model.rooms:
+        if room.zone == room.identifier:
+            room.zone = '{} Zone'.format(room.identifier)
+
+    # set the component defaults
     name = _name_ if _name_ is not None else _model.identifier
     lower_name = name.lower()
     gbxml_file = name if lower_name.endswith('.xml') or lower_name.endswith('.gbxml') \
         else '{}.xml'.format(name)
     folder = _folder_ if _folder_ is not None else folders.default_simulation_folder
     gbxml = os.path.join(folder, gbxml_file)
-    triangulate_subfaces = True if triangulate_ else False
-    full_geometry = True if full_geo_ else False
-    interior_face_type = 'InteriorFloor' if int_floors_ else None
+    ip_units = bool(ip_units_)
+    full_geo = bool(full_geo_)
+    rect_geo_format = 'BoundingRectangle' if _rect_format_ is None else _rect_format_
     prog_name = 'Ladybug Tools for Grasshopper'
     lbt_gh = lbr_folders.lbt_grasshopper_version_str
 
     # write the Model to a gbXML file
-    if model_to_gbxml is not None:  # run the whole translation in IronPython
-        gbxml_str = model_to_gbxml(
-            _model, triangulate_subfaces=triangulate_subfaces,
-            full_geometry=full_geometry, interior_face_type=interior_face_type,
-            program_name=prog_name, program_version=lbr_folders.lbt_grasshopper_version_str
-        )
-        with open(gbxml, 'w') as outf:
-            outf.write(gbxml_str)
-    else:  # do the translation using cPython through the CLI
-        # write the model to a HBJSON
-        temp_dir = tempfile.gettempdir()
-        model_file = os.path.join(temp_dir, 'in.hbjson')
-        with open(model_file, 'w') as fp:
-            model_str = json.dumps(_model.to_dict(), ensure_ascii=False)
-            fp.write(model_str.encode('utf-8'))
-        # execute the command to convert the HBJSON to gbXML
-        cmds = [folders.python_exe_path, '-m', 'honeybee_energy', 'translate',
-                'model-to-gbxml', model_file, '--output-file', gbxml]
-        if triangulate_subfaces:
-            cmds.append('--triangulate-subfaces')
-        if full_geometry:
-            cmds.append('--full-geometry')
-        if int_floors_:
-            cmds.append('--interior-face-type')
-            cmds.append(interior_face_type)
-        cmds.append('--program-name')
-        cmds.append(prog_name)
-        if lbr_folders.lbt_grasshopper_version_str is not None:
-            cmds.append('--program-version')
-            cmds.append(lbr_folders.lbt_grasshopper_version_str)
-        custom_env = os.environ.copy()
-        custom_env['PYTHONHOME'] = ''
-        use_shell = True if os.name == 'nt' else False
-        process = subprocess.Popen(cmds, shell=use_shell, env=custom_env)
-        result = process.communicate()  # freeze the canvas while running
+    gbxml_str = model_to_gbxml(
+        model, ip_units=ip_units,
+        include_shell_geometry=full_geo, include_space_boundaries=full_geo,
+        rect_geo_format=rect_geo_format,
+        program_name=prog_name, program_version=lbr_folders.lbt_grasshopper_version_str
+    )
+    gbxml_str = '<?xml version="1.0" encoding="utf-8"?>\n' + gbxml_str
+    with open(gbxml, 'w') as outf:
+        outf.write(gbxml_str)
